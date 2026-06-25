d85403a6780be60f35bee4c5f5349f21740e49b2
Xyrelix <officialoladayooladoyin@gmail.com>
2026-06-25 11:50:14 +0100

    fix: restore hash.js and token.js utils lost during merge

--- src/common/utils/hash.js
+++ src/common/utils/hash.js
@@ -1,2 +1,10 @@
-// bcrypt hash/compare
-// TODO: implement
+// src/common/utils/hash.js
+import bcrypt from 'bcrypt';
+
+const SALT_ROUNDS = 10;
+
+export const hashPassword = async (plain) => bcrypt.hash(plain, SALT_ROUNDS);
+
+export const comparePassword = async (plain, hashed) => bcrypt.compare(plain, hashed);
+
+export default { hashPassword, comparePassword };
